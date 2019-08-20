a56f038a493c7335617be4af52feb26034181baf
Armand <dongmoarmand@gmail.com>
2019-08-20 19:00:52 +0100

    Used attr_readers/attr_writers instead of atrr_acessors.

--- bin/main.rb
+++ bin/main.rb
@@ -5,7 +5,7 @@ require_relative '../lib/game_logic'
 
 
 board= Board.new
-user_one = Player.new('user_one','O').name
-user_two = Player.new('user_two','T').name
+user_one = Player.new('user_one','O') 
+user_two = Player.new('user_two','T') 
 
 GameLogic.new(user_one, user_two, board).game_play
